3f6031ade186245ce1cb99660b7ce61ae6e328e5
kentin <quentin.bregeon23@gmail.com>
2020-03-09 16:22:45 +0100

    first

---
+++ src/components/MenuBurger.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+              <v-list-item to="/">Home page</v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Mes Scénarios</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+              <v-list-item to="/profil">Profil</v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Paramètres</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Menu</v-toolbar-title>
+    </v-app-bar>
+    <router-view></router-view>
+    <v-footer app>
+      <span>&copy; Storyline Project</span>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
